ac86871d8a0f0525fa51eb406674e21c3063e34a
David Vollbracht <david.vollbracht@gmail.com>
2011-03-14 23:45:59 -0400

    Add config/initializers/omniauth.rb with Google OpenID setup

---
+++ library/config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+require 'openid/store/filesystem'
+Rails.application.config.middleware.use OmniAuth::Strategies::OpenID,
+                                        OpenID::Store::Filesystem.new('/tmp/library/openid'),
+                                        :name => 'google',
+                                        :identifier => 'https://www.google.com/accounts/o8/id'
+
