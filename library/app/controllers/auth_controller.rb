365dc50306d98910349eb5aa83ef537fe7dc0665
David Vollbracht <david.vollbracht@gmail.com>
2011-03-15 20:01:04 -0400

    Don't create duplicate identities

--- library/app/controllers/auth_controller.rb
+++ library/app/controllers/auth_controller.rb
@@ -1,8 +1,14 @@
 class AuthController < ApplicationController
   def callback
-    @identity = Identity.create! uid: request.env['omniauth.auth']['uid'],
-                                 provider: request.env['omniauth.auth']['provider'],
-                                 name: request.env['omniauth.auth']['user_info']['name']
+    @identity = Identity.where(:uid => request.env['omniauth.auth']['uid'],
+                               :provider => request.env['omniauth.auth']['provider']).first
+
+    if @identity.nil?
+      @identity = Identity.create! :uid => request.env['omniauth.auth']['uid'],
+                                   :provider => request.env['omniauth.auth']['provider'],
+                                   :name =>request.env['omniauth.auth']['user_info']['name']
+      @new_identity = true
+    end
     session[:identity_id] = @identity.id
   end
 end
