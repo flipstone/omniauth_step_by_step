32e662a621fe694d4b368e232cbdd26ae47f3a40
David Vollbracht <david.vollbracht@gmail.com>
2011-03-15 20:14:16 -0400

    Establish identity before every request, show identity and nav in layout

--- library/app/controllers/application_controller.rb
+++ library/app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :establish_identity
+
+  protected
+
+  def establish_identity
+    if session[:identity_id]
+      @identity = Identity.where(id: session[:identity_id]).first
+    end
+  end
 end
